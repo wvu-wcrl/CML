d1d49868ec61c988a52652eb7bf3a7870275ff36
terry.ferrett <terry.ferrett@gmail.com>
2012-07-30 02:45:26 +0000

    updated cml task creation test

--- test/pcs/t_cml_task_creation.m
+++ test/pcs/t_cml_task_creation.m
@@ -10,12 +10,23 @@ cml_home_remote = ['/rhome' path_suffix];
 FunctionPath = [cml_home_remote '/' 'mat'];
 FunctionName = 'CmlWorker';
 
+% read code_param short from disk
+load('code_param_short.mat');
+
+
+% copy code_param_long example to data directory  
+load('code_param_long.mat');
+save('/home/tferrett/Projects/cml/data/t_BerSim_2.mat');
+
 
 % create TaskParam
 clear TaskParam;
 TaskParam.FunctionName = FunctionName;
 TaskParam.FunctionPath = FunctionPath;
 TaskParam.InputParam.JobParam = sim_param;
+TaskParam.InputParam.JobParam.scenario = 't_BerSim';
+TaskParam.InputParam.JobParam.record = 2;
+TaskParam.InputParam.JobParam.code_param_short = code_param_short;
 TaskParam.InputParam.JobParam.cml_home = cml_home_remote;
 TaskParam.InputParam.JobParam.RandSeed = 100;
 TaskParam.InputParam.JobState = sim_state;
