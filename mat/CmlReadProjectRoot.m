5452e4f27c238b4caf9f608b5001bea0f74b4691
terry.ferrett <terry.ferrett@gmail.com>
2012-08-14 04:02:45 +0000

    added function to cml to read cluster cml configuration file

---
+++ mat/CmlReadProjectRoot.m
@@ -0,0 +1,70 @@
+% CmlReadProjectRoot.m
+%
+%  Read cluster CML job queue root.
+%
+%
+% Inputs
+%
+%     Last updated on 8/13/2012
+%
+%     Copyright (C) 2012, Terry Ferrett and Matthew C. Valenti
+%     For full copyright information see the bottom of this file.
+
+
+
+function [ project_root ] = CmlReadProjectRoot()
+
+util_path = '/home/pcs/util';   % add path to file reading utility
+addpath(util_path);
+
+cml_proj_cf = get_proj_cf();     % path to user cml project file
+
+% get path to cml
+heading = '[GeneralSpec]';
+key = 'CodeRoot';
+out = util.fp(cml_proj_cf, heading, key);
+cml_home = out{1}{1};
+
+addpath(cml_home);
+
+% get path to cml project root
+heading = '[GeneralSpec]';
+key = 'JobQueueRoot';
+out = util.fp(cml_proj_cf, heading, key);
+project_root = out{1}{1};
+project_root = project_root(1:end-1);  %TEMP
+
+end
+
+
+function cf_path = get_proj_cf()    % get config file for this user
+
+  user = get_current_user();
+
+cml_proj_cf = 'cml_cfg';
+cf_path = ['/home' '/' user '/' cml_proj_cf];
+end
+
+
+function user = get_current_user()                                        
+  [dontcare user] = system('whoami');                                     
+user = user(1:end-1);                                                   
+end                                                                       
+        
+
+
+
+%     This library is free software;
+%     you can redistribute it and/or modify it under the terms of
+%     the GNU Lesser General Public License as published by the
+%     Free Software Foundation; either version 2.1 of the License,
+%     or (at your option) any later version.
+%
+%     This library is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%     Lesser General Public License for more details.
+%
+%     You should have received a copy of the GNU Lesser General Public
+%     License along with this library; if not, write to the Free Software
+%     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
