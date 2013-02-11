89ea9c24bc80c4118a109b7fa192f98c3833afcb
terry.ferrett <terry.ferrett@gmail.com>
2013-02-11 03:28:11 +0000

    updated CML LDPC decoder makefile

--- module/chan_code/ldpc/decoder/src/mexsrc/make.m
+++ module/chan_code/ldpc/decoder/src/mexsrc/make.m
@@ -1,2 +1,23 @@
-mex  -I./lib InitState_mx.c ./lib/ldpc-util.c ./lib/math-supp.c 
-mex  -I./lib Iterate_mx.c ./lib/ldpc-util.c ./lib/math-supp.c
+% makefile for LDPC decoder
+%
+% Last updated Feb 10th, 2013
+
+
+% clear mex files
+clear InitState_mx.c
+clear Iterate_mx.c
+
+
+% if linux, delete files
+if isunix,
+    delete ../mex/*
+end
+
+
+if ispc,
+    mex  -output ../mex/InitState_mx.dll -I./lib InitState_mx.c ./lib/ldpc-util.c ./lib/math-supp.c
+    mex  -output ../mex/Iterate_mx.dll -I./lib Iterate_mx.c ./lib/ldpc-util.c ./lib/math-supp.c
+else
+    mex  -output ../mex/InitState_mx -I./lib InitState_mx.c ./lib/ldpc-util.c ./lib/math-supp.c
+    mex  -output ../mex/Iterate_mx -I./lib Iterate_mx.c ./lib/ldpc-util.c ./lib/math-supp.c
+end
