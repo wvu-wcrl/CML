8a124b32d6877bc2bc2ed58998e0f1ac5a2a41b7
terry.ferrett <terry.ferrett@gmail.com>
2013-02-12 00:03:59 +0000

    bug fixes in cml parallel computing functionality

--- mat/parallel/cluster/GetProjCf.m
+++ mat/parallel/cluster/GetProjCf.m
@@ -1,6 +1,6 @@
 function cf_path = GetProjCf()    % get config file for this user
 
-  user = get_current_user();
+  user = GetCurrentUser();
 
 cml_proj_cf = 'cml_cfg';
 cf_path = ['/home' '/' user '/' cml_proj_cf];
