bfefe01b151ba38f1b3fc7b5c24fc2192125b695
terry.ferrett <terry.ferrett@gmail.com>
2013-02-28 04:51:27 +0000

    added additional records to SpeedProfile scenario - K=9 NSC conv code; single, high-snr, high trial sim

--- scenarios/test/SpeedProfile.m
+++ scenarios/test/SpeedProfile.m
@@ -4,7 +4,19 @@
 % speed comparisons between standalone and cluster
 % cml.
 %
+%
+%  = Description of Tests=
+%  Record      Description
+%  1           short running ( < 20s )test for functionality
+%  2           moderate running ( ~ x min ) test
+%  3           long running ( ~ x min ) test
+%  4           conv code, K=7
+%  5           conv code, K=9
+%  6           single high SNR, high trials
+%
 % Last updated on 10 22 2012
+
+
 
 % determine where your root directory is
 load( 'CmlHome.mat' );
@@ -43,7 +55,7 @@ sim_param(record).max_trials = 1e9*ones( size(sim_param(record).SNR) );
 sim_param(record).minBER = 1e-6; 
 sim_param(record).max_frame_errors = 200*ones( size(sim_param(record).SNR) );
 sim_param(record).save_rate = 100;
-sim_param(record).MaxRunTime = 3*60; 
+sim_param(record).MaxRunTime = 60; 
 sim_param(record).timing_sample_rate = 30;
 sim_param(record).ProfileSpeed = 1;
 
@@ -69,7 +81,7 @@ sim_param(record).max_trials = 1e9*ones( size(sim_param(record).SNR) );
 sim_param(record).minBER = 1e-7; 
 sim_param(record).max_frame_errors = 200*ones( size(sim_param(record).SNR) );
 sim_param(record).save_rate = 100;
-sim_param(record).MaxRunTime = 3*60; 
+sim_param(record).MaxRunTime = 60; 
 sim_param(record).timing_sample_rate = 60; 
 sim_param(record).ProfileSpeed = 1;
 
@@ -108,14 +120,14 @@ sim_param(record).nsc_flag1 = 0;
 sim_param(record).nsc_flag2 = 0;
 sim_param(record).code_interleaver = ...
   strcat( 'CreateUmtsInterleaver(', int2str(sim_param(record).framesize ), ')' );
-sim_param(record).MaxRunTime = 3*60; 
+sim_param(record).MaxRunTime = 60; 
 sim_param(record).timing_sample_rate = 60;
 sim_param(record).ProfileSpeed = 1;
 
 
 % convolutional code
 record = 4;
-sim_param(record).comment = 'long runtime.  convolutional code';
+sim_param(record).comment = 'long runtime. NSC, R=1/2, K=7';
 sim_param(record).sim_type = 'coded';
 sim_param(record).code_configuration = 0;
 sim_param(record).SNR = [0:0.5:8];
@@ -141,5 +153,71 @@ sim_param(record).max_trials = 1e9*ones( size(sim_param(record).SNR) );
 sim_param(record).minBER = 1e-6; 
 sim_param(record).max_frame_errors = 30*ones( size(sim_param(record).SNR) );
 sim_param(record).save_rate = 50;
-sim_param(record).MaxRunTime = 3*60; 
+sim_param(record).MaxRunTime = 60; 
+sim_param(record).ProfileSpeed = 1;
+
+
+
+% convolutional code
+record = 5;
+sim_param(record).comment = 'long runtime.  NSC, R=1/2, K=9';
+sim_param(record).sim_type = 'coded';
+sim_param(record).code_configuration = 0;
+sim_param(record).SNR = [0:0.5:8];
+sim_param(record).SNR_type = 'Eb/No in dB';
+sim_param(record).framesize = 5114; 
+sim_param(record).modulation = 'BPSK';
+sim_param(record).mod_order = 2;
+sim_param(record).mapping = [];
+sim_param(record).channel = 'AWGN';
+sim_param(record).bicm = 1;
+sim_param(record).demod_type = 0; 
+sim_param(record).linetype = 'k:';
+sim_param(record).legend = sim_param(record).comment;
+sim_param(record).g1 = [ 1 0 1 1 1 0 0 0 1
+   1 1 1 1 0 1 0 1 1 ];
+sim_param(record).nsc_flag1 = 1;
+sim_param(record).pun_pattern1 = [];
+sim_param(record).tail_pattern1 = [];
+sim_param(record).decoder_type = 0;
+sim_param(record).filename = strcat( data_directory, 't_bersim_5.mat');
+sim_param(record).reset = 0;
+sim_param(record).max_trials = 1e9*ones( size(sim_param(record).SNR) );
+sim_param(record).minBER = 1e-6; 
+sim_param(record).max_frame_errors = 30*ones( size(sim_param(record).SNR) );
+sim_param(record).save_rate = 50;
+sim_param(record).MaxRunTime = 60; 
+sim_param(record).ProfileSpeed = 1;
+
+
+
+% convolutional code
+record = 6;
+sim_param(record).comment = 'long runtime.  NSC, R=1/2, K=9';
+sim_param(record).sim_type = 'coded';
+sim_param(record).code_configuration = 0;
+sim_param(record).SNR = [70];
+sim_param(record).SNR_type = 'Eb/No in dB';
+sim_param(record).framesize = 5114; 
+sim_param(record).modulation = 'BPSK';
+sim_param(record).mod_order = 2;
+sim_param(record).mapping = [];
+sim_param(record).channel = 'AWGN';
+sim_param(record).bicm = 1;
+sim_param(record).demod_type = 0; 
+sim_param(record).linetype = 'k:';
+sim_param(record).legend = sim_param(record).comment;
+sim_param(record).g1 = [ 1 0 1 1 1 0 0 0 1
+   1 1 1 1 0 1 0 1 1 ];
+sim_param(record).nsc_flag1 = 1;
+sim_param(record).pun_pattern1 = [];
+sim_param(record).tail_pattern1 = [];
+sim_param(record).decoder_type = 0;
+sim_param(record).filename = strcat( data_directory, 't_bersim_6.mat');
+sim_param(record).reset = 0;
+sim_param(record).max_trials = 1e13*ones( size(sim_param(record).SNR) );
+sim_param(record).minBER = 1e-20; 
+sim_param(record).max_frame_errors = 3000*ones( size(sim_param(record).SNR) );
+sim_param(record).save_rate = 100;
+sim_param(record).MaxRunTime = 60; 
 sim_param(record).ProfileSpeed = 1;
